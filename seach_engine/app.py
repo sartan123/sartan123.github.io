d19ccbc863d65b0485489966d44e17f968c7d07e
sartan123 <sartan8827@gmail.com>
2017-11-04 21:03:48 +0900

    upload
    upload seach engine

---
+++ seach_engine/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+file = open('result.json', 'r')
+result = json.load(file)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """Return index.html
+    """
+    if request.method == 'POST':
+        keyword = request.form['keyword']
+        if keyword:
+            return render_template(
+                'index.html',
+                query=result[keyword],
+                keyword=keyword)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True  # デバッグモード有効化
+    app.run(host='0.0.0.0')  # どこからでもアクセス可能に
